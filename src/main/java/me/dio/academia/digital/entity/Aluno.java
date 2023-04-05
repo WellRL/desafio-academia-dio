1a3c96c274f99c80f658032d6174239755795fff
WellRL <well2.1@outlook.com>
2023-04-05 18:06:37 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -0,0 +1,32 @@
+package me.dio.academia.digital.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "hsndler"})
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String cpf;
+    private String bairro;
+    private LocalDate dataDeNascimento;
+
+    @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+
+}
